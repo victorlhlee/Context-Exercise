9b195ddd5b00c1789b2f3465f9b0ac5c3382f1e0
Jon Borgonia <jon@gomagames.com>
2014-09-20 15:15:53 -1000

    instructions and js

---
+++ context.js
@@ -0,0 +1,30 @@
+var Robot = function (name) {
+  this.name = name;
+
+}
+
+function add ( op1, op2 ) {
+  this.name = this.name || "Humans";
+  return this.name + " can count to " + (op1 + op2);
+}
+
+var voltron = new Robot("Voltron");
+var bender = new Robot("Bender Rodriguez");
+var optimus = new Robot("Optimus Prime");
+var megaman = new Robot("Mega Man");
+var bmo = new Robot("B-Mo");
+var wall_e = new Robot("Wall-E");
+
+
+// #1
+console.log(  );
+
+// #2  
+console.log(  );
+
+// #3
+console.log(  );
+
+// #4
+
+console.log(  );
